58231ffe23fd9a08526d6b1e4c3facff7bec283a
Victor Dominoni <dominoni.victor@gmail.com>
2019-05-29 19:25:27 -0300

    Removi fopen write e adicionei leitura de multiplas linhas (fixo)

--- ep.cpp
+++ ep.cpp
@@ -8,28 +8,21 @@ using namespace std;
 
 int main(){
     
-    int linhas, colunas, nPalavras, i=0;
-    char info[10];
+    int linhas, colunas, nPalavras, i=0, n=6;
     FILE * arquivo;
+    char info1[n], info2[n], info3[n];
     
-    //Abre um arquivo no modo "escrever" (write)
-    arquivo = fopen("epdoc.txt", "w");
-    //Checa se o arquivo existe mesmo (talvez seja redundate visto qe o fopen cria o arquivo caso nao exista)
-    if(arquivo != NULL){
-        //Adiciona o texto abaixo no arquivo
-        fputs("3 2 1 \nabc", arquivo);
-        fclose(arquivo);
-    }
-    //Abre o arquivo novamente, porém no modo "leitura" (read)
+	//Abre o arquivo no modo "leitura" (read)
     arquivo = fopen("epdoc.txt", "r");
     //Checa se o arquivo existe
     if(arquivo != NULL){
         //Coleta as informações do arquivo seguindo esse padrao
-    	fscanf(arquivo, "%d %d %d\n%s", &linhas, &colunas, &nPalavras, info);
+    	fscanf(arquivo, "%d %d %d\n", &linhas, &colunas, &nPalavras);//1a linha com inteiros
+    	fscanf(arquivo, "%s\n%s\n%s", info1, info2, info3);//2a, 3a e 4a linha com strings
     	fclose(arquivo);	
 	}
     //Imprime o string de characters e o 3o numero do arquivo
-	printf(info);
+	cout << info1 << "\n" << info2 << "\n" << info3;
 	printf("\n%d", nPalavras);
     //Finaliza o programa
     return 0;
