3d9b5826f1d4f15141292f69622e19351b507927
Victor Dominoni <dominoni.victor@gmail.com>
2019-06-01 11:32:41 -0300

    Reordenado o codigo para usar funções ao invés de rodar tudo no int main()

--- ep.cpp
+++ ep.cpp
@@ -3,50 +3,57 @@
 #include <iostream>
 #include <string>
 
+//definindo constantes
+#define MAX_CHAR 18
+#define MAX_PAL 10
+#define MAX_MAT 30
+
+//Inicializando variaveis
+int linhas, colunas, nPalavras;
+char matriz[MAX_MAT][MAX_MAT], palavras[MAX_PAL][MAX_CHAR];
+bool deuErro = false;
 
 using namespace std;
 
-int main(){
+void leDoc(){
 	//Inicializando as variaveis
-    int linhas, colunas, nPalavras, max_char=14;
-    FILE * arquivo;
-    char nomeArquivo[20] = "epdoc.txt";
-    cout << "Insira o nome do arquivo completo (com .txt) caça palavras (note qe o arquivo deve estar com a formatação adequada):\n";
+    char nomeArquivo[20];
+    FILE* arquivo;
+    cout << "Insira o nome do arquivo completo (com .txt) do caça palavras (note qe o arquivo deve estar com a formatacao adequada, indicado no arquivo READ.ME):\n";
     cin >> nomeArquivo;
 	//Abre o arquivo no modo "leitura" (read)
     arquivo = fopen(nomeArquivo, "r");
     //Checa se o arquivo existe
     if(arquivo == NULL){
         cout << "Não foi possivel carregar o arquivo,  cheque se o nome e o arquivo estão corretos";
-        return 0;
 	}else{
 		//Coleta as informacoes do arquivo seguindo esse padrao
     	fscanf(arquivo, "%d %d %d\n", &linhas, &colunas, &nPalavras);//1a linha com inteiros	
-	}
-	char info[linhas][colunas];
-    char palavras[nPalavras][max_char];
-    
-	//Coleta o caca palavras e salva na matriz
-	for(int j = 0; j < linhas; j++){
-		for(int k = 0; k < colunas; k++){
-			char st;
-			if(k==colunas-1){
-				fscanf(arquivo, "%c\n", &st);
-			}else{
-				fscanf(arquivo, "%c", &st);	
+		//Coleta o caca palavras e salva na matriz
+		for(int j = 0; j < linhas; j++){
+			for(int k = 0; k < colunas; k++){
+				char st;
+				if(k==colunas-1){
+					fscanf(arquivo, "%c\n", &st);
+				}else{
+					fscanf(arquivo, "%c", &st);	
+				}
+				matriz[j][k] = st;
 			}
-			info[j][k] = st;
 		}
+		//le palavras
+		int i = 0;
+		char temp[MAX_CHAR];
+		do { //le a linha de palavras e salva em temp
+			fgets(temp, sizeof (temp), arquivo);
+			strcpy(palavras[i],temp); // LEMBRE QE O PALAVRAS[i] é salvo com o "\n" no final!
+			i++;
+		}while (i < nPalavras);
+		fclose(arquivo);
 	}
-	//le palavras
-	int i = 0;
-	char temp[max_char];
-	do { //le a linha de palavras e salva em temp
-		fgets(temp, sizeof (temp), arquivo);
-		strcpy(palavras[i],temp); // LEMBRE QE O PALAVRAS[i] é salvo com o "\n" no final!
-		i++;
-	}while (i < nPalavras);
-	//Imprime a matriz (info) devidamente, com os numeros e com a formatação adequada
+}
+
+void imprime(){
 	for(int j = 0; j < linhas + 2; j++) {
 		cout << "\t\t\t\t";
         for(int k = 0; k < colunas + 2; k++) {
@@ -64,26 +71,17 @@ int main(){
 				} else if(k == colunas + 1) {//se for a ultima coluna
 					cout << j - 1 << endl;
 				} else { //se nao for nenhuma das anteriores, então imprimiremos a matriz
-				   cout << info[j-1][k-1] << " ";
+				   cout << matriz[j-1][k-1] << " ";
                }       
 		}  
-    }    
+    } 
+}
+
+int main(){
+	//Le o documento e salva toda sua informação
+	leDoc();
+	//Imprime a matriz (info) devidamente, com os numeros e com a formatação adequada
+	imprime();   
     
-	
-	
-    /*Imprime o string de characters e o 3o numero do arquivo
-    for(int j = 0; j < linhas; j++){
-		for(int k = 0; k < colunas; k++){
-    		cout << info[j][k] << " " << j << ", " << k << "\n";
-		}
-		cout << "\n";
-	}
-	//Imprime palavras
-	for(int i = 0; i<nPalavras ; i++){
-		cout << palavras[i];
-	}
-	printf("\n%d", nPalavras);
-    //Finaliza o programa
-    */
 	return 0;
 }
